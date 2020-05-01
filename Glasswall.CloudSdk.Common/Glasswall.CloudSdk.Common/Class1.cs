cbf4bc8ad78b3b7322e599fe11d1ca2aa5897bf2
gw-sbirch <60774443+gw-sbirch@users.noreply.github.com>
2020-05-01 10:56:35 +0100

    add nuget project

---
+++ Glasswall.CloudSdk.Common/Glasswall.CloudSdk.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Glasswall.CloudSdk.Common
+{
+    public class TestClass
+    {
+        public string TestMethod()
+        {
+            return "Hello, World!";
+        }
+    }
+}
